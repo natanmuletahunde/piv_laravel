dbd8c84e65b7c0dba5d3cde3631a4f8750747070
pivnati <natanmuleta911@gmail.com>
2024-09-08 18:34:25 -0700

    create the logout in the admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,5 +7,10 @@
 </head>
 <body>
        <h1>ADMIN</h1>
+
+       <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                <input type="submit" value="logout">
+                        </form>
 </body>
 </html>
