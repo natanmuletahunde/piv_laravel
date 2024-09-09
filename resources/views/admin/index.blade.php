95c6ad652040c121776b4c275f73f3b67d3ded8c
pivnati <natanmuleta911@gmail.com>
2024-09-09 14:07:22 -0700

    create the css for the images

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,25 +9,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
     
-    <!-- Bootstrap CSS-->
+ 
     <link rel="stylesheet" href="{{asset('/admincss/vendor/bootstrap/css/bootstrap.min.css')}}">
-    
-    <!-- Font Awesome CSS-->
+  
     <link rel="stylesheet" href="{{asset('/admincss/vendor/font-awesome/css/font-awesome.min.css')}}">
     
-    <!-- Custom Font Icons CSS-->
     <link rel="stylesheet" href="{{asset('/admincss/css/font.css')}}">
     
-    <!-- Google fonts - Muli-->
+   
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
     
-    <!-- Theme stylesheet -->
+  
     <link rel="stylesheet" href="{{asset('/admincss/css/style.default.css')}}" id="theme-stylesheet"> <!-- Corrected this line -->
     
-    <!-- Custom stylesheet - for your changes-->
+ 
     <link rel="stylesheet" href="{{asset('/admincss/css/custom.css')}}">
     
-    <!-- Favicon-->
+   
     <link rel="shortcut icon" href="{{asset('/admincss/img/favicon.ico')}}">
     
     <!-- Tweaks for older IEs-->
@@ -196,7 +194,7 @@
         <nav id="sidebar">
             <!-- Sidebar Header-->
             <div class="sidebar-header d-flex align-items-center">
-                <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
+                <div class="avatar"><img src="{{asset('/admincss/img/avatar-6.jpg')}}" alt="..." class="img-fluid rounded-circle"></div>
                 <div class="title">
                     <h1 class="h5">Mark Stephen</h1>
                     <p>Web Designer</p>
