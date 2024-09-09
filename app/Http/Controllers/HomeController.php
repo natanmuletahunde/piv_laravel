5d2c5cabe54526385a142a441963ee6328d33749
pivnati <natanmuleta911@gmail.com>
2024-09-08 20:58:22 -0700

    make the  homepage to homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view("admin.index");
+    }
+
+    public function home()
+    {
+        return view("home.index");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [HomeController::class,'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
