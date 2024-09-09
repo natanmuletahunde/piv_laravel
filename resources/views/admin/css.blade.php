93a2e17368666785d415317927657316288b9aef
pivnati <natanmuleta911@gmail.com>
2024-09-09 14:18:08 -0700

    create the css.blade.php inside the admin folder

---
+++ resources/views/admin/css.blade.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dark Bootstrap Admin</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    
+ 
+    <link rel="stylesheet" href="{{asset('/admincss/vendor/bootstrap/css/bootstrap.min.css')}}">
+  
+    <link rel="stylesheet" href="{{asset('/admincss/vendor/font-awesome/css/font-awesome.min.css')}}">
+    
+    <link rel="stylesheet" href="{{asset('/admincss/css/font.css')}}">
+    
+   
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
+    
+  
+    <link rel="stylesheet" href="{{asset('/admincss/css/style.default.css')}}" id="theme-stylesheet"> <!-- Corrected this line -->
+    
+ 
+    <link rel="stylesheet" href="{{asset('/admincss/css/custom.css')}}">
+    
+   
+    <link rel="shortcut icon" href="{{asset('/admincss/img/favicon.ico')}}">
