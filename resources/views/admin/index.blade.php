9bc943869c5f44f61ca8a7dac9e0a91bbf1a99c0
pivnati <natanmuleta911@gmail.com>
2024-09-09 13:45:37 -0700

    make the script to public

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -609,10 +609,10 @@
             </footer>
         </div>
     </div>
-    <!-- JavaScript files-->
+  
     <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script> <!-- Corrected this line -->
+    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script> 
     <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"></script>
     <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
     <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
