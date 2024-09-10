09cbbb1026182e9d717a6d4cc054b74e0899a002
pivnati <natanmuleta911@gmail.com>
2024-09-10 11:30:41 -0700

    create the conformation delete in the category.blade.php

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -2,104 +2,113 @@
 <html>
 
 <head>
-@include('admin.css');
+    @include('admin.css');
 
 
-<style type="text/css">
+    <style type="text/css">
+        input[type=text] {
+            width: 400px;
+            height: 50px;
+        }
 
-    input[type=text]{
-        width: 400px;
-        height: 50px;
-    }
-    .div_deg
-    {
-       display: flex;
-       justify-content: center;
-       align-items: center;
-       margin: 30px;;
-    }
-    .table_deg
-    {
-text-align: center;
-margin: auto;
-border: 2px solid yellowgreen;
-margin-top: 50px;
-width: 600px;
-    }
-th{
-    background-color: skyblue;
-    padding: 15px;
-    font-size: 20px;
-    font-weight: bold;
-    color:white;
-}
-td
-{
-    padding: 10px;
-    color:white ;
-    border:1px solid skyblue;
-}
+        .div_deg {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 30px;
+            ;
+        }
 
-</style>
+        .table_deg {
+            text-align: center;
+            margin: auto;
+            border: 2px solid yellowgreen;
+            margin-top: 50px;
+            width: 600px;
+        }
+
+        th {
+            background-color: skyblue;
+            padding: 15px;
+            font-size: 20px;
+            font-weight: bold;
+            color: white;
+        }
+
+        td {
+            padding: 10px;
+            color: white;
+            border: 1px solid skyblue;
+        }
+    </style>
 </head>
+
 <body>
     @include('admin.header');
     <div class="d-flex align-items-stretch">
         <!-- Sidebar Navigation-->
-         @include('admin.sidebar');
+        @include('admin.sidebar');
         <!-- Sidebar Navigation end-->
         <div class="page-content">
             <div class="page-header">
                 <div class="container-fluid">
-                <h1 style="color:white;">Add Category</h1>
-                <div class="div_deg">
-                   
-                  <form  action="{{url('add_category')}}" method="post">
-                    @csrf
-                   <div>
-                    <input type="text" name="category">
-                      <input class="btn btn-primary" type="submit" value="Add category">
-                   </div>
-                  </form>
-                  </div>
-               
-                  <div>
+                    <h1 style="color:white;">Add Category</h1>
+                    <div class="div_deg">
 
-                    <table class="table_deg">
-                        <tr>
-                    <th>
-                        Category Name
-                    </th>
+                        <form action="{{url('add_category')}}" method="post">
+                            @csrf
+                            <div>
+                                <input type="text" name="category">
+                                <input class="btn btn-primary" type="submit" value="Add category">
+                            </div>
+                        </form>
+                    </div>
 
-                    <th>Delete</th>
-                        </tr>
+                    <div>
 
-                       @foreach ($data as $data)
-                       
-                      
-                        <tr>
-                            <td>{{$data->category_name}}</td>
-                            <td>
-                                <a class="btn btn-danger" href="{{url('delete_category',$data->id)}}"> Delete</a>
-                            </td>
-                        </tr>
-                       @endforeach
-                    </table>
+                        <table class="table_deg">
+                            <tr>
+                                <th>
+                                    Category Name
+                                </th>
 
-                  </div>
+                                <th>Delete</th>
+                            </tr>
 
+                            @foreach ($data as $data)
+
+
+                            <tr>
+                                <td>{{$data->category_name}}</td>
+                                <td>
+                                    <a class="btn btn-danger" onclick="confirmation(event)"  href="{{url('delete_category',$data->id)}}"> Delete</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
+
+                    </div>
+
+                </div>
             </div>
         </div>
-    </div>
-  
-    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script> 
-    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"></script>
-    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
-    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
-    <script src="{{asset('admincss/js/front.js')}}"></script>
+
+        <script type="text/javascript">
+            function confirmation(ev){
+              ev.preventDefault();
+              var urlToRedirect = ev.currentTarget.getAttribute('href');
+            }
+        </script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"></script>
+        <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"></script>
+        <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+        <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+        <script src="{{asset('admincss/js/charts-home.js')}}"></script>
+        <script src="{{asset('admincss/js/front.js')}}"></script>
 
 </body>
 
