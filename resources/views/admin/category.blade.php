1fa7569183b1b9ea3cc07122ed95d9c57d1dad55
pivnati <natanmuleta911@gmail.com>
2024-09-09 18:11:24 -0700

    modify the add category page in the admin folder

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+@include('admin.css');
 </head>
 <body>
-       <h1>Category</h1>
+    @include('admin.header');
+    <div class="d-flex align-items-stretch">
+        <!-- Sidebar Navigation-->
+         @include('admin.sidebar');
+        <!-- Sidebar Navigation end-->
+        <div class="page-content">
+            <div class="page-header">
+                <div class="container-fluid">
+                  <form >
+                   <div>
+                    <input type="text" name="category">
+                   </div>
+                   <div>
+                      <input class="btn btn-primary" type="submit" value="Add category">
+                   </div>
+                  </form>
+            </div>
+        </div>
+    </div>
+  
+    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script> 
+    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"></script>
+    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
+    <script src="{{asset('admincss/js/front.js')}}"></script>
+
 </body>
+
 </html>
