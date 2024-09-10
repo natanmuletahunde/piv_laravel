05d191d2d131ccdd0018814758dee07df3c35284
pivnati <natanmuleta911@gmail.com>
2024-09-10 12:27:23 -0700

    cretate the  post action in the edit_category

--- resources/views/admin/edit_category.blade.php
+++ resources/views/admin/edit_category.blade.php
@@ -2,53 +2,59 @@
 <html>
 
 <head>
-@include('admin.css');
+    @include('admin.css');
 
-<style>
-
-    .div_deg{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 60px;
-    }
-</style>
+    <style>
+        .div_deg {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 60px;
+        }
+        input[type="text"]
+        {
+            width: 400px;
+            height: 50px;
+        }
+    </style>
 </head>
+
 <body>
     @include('admin.header');
     <div class="d-flex align-items-stretch">
         <!-- Sidebar Navigation-->
-         @include('admin.sidebar');
+        @include('admin.sidebar');
         <!-- Sidebar Navigation end-->
         <div class="page-content">
             <div class="page-header">
                 <div class="container-fluid">
-               <div class="div_deg">
-        <h1>
-            Update Category
-        </h1>
-        <form >
-            <input type="text" name="category" value="{{$data->category_name}}">
 
-            <input class="btn btn-primary" type="Submit" name="category" value="Update Category">
+                    <h1 style="color:white;">
+                        Update Category
+                    </h1>
+                    <div class="div_deg">
 
-            
-        </form>
+                        <form action="{{url('update_category',$data->id)}}" method="post">
+                              @csrf
+                            <input type="text" name="category" value="{{$data->category_name}}">
 
-               </div>
-                
+                            <input class="btn btn-primary" type="Submit" name="category" value="Update Category">
+                        </form>
+
+                    </div>
+
+                </div>
             </div>
         </div>
-    </div>
-  
-    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script> 
-    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"></script>
-    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
-    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
-    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
-    <script src="{{asset('admincss/js/front.js')}}"></script>
+
+        <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"></script>
+        <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"></script>
+        <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+        <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+        <script src="{{asset('admincss/js/charts-home.js')}}"></script>
+        <script src="{{asset('admincss/js/front.js')}}"></script>
 
 </body>
 
