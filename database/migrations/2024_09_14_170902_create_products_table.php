66fe1b29e3fea814b1d33311f7bc6f2c7a5cff98
pivnati <natanmuleta911@gmail.com>
2024-09-14 12:02:42 -0700

    update the table in the created

--- database/migrations/2024_09_14_170902_create_products_table.php
+++ database/migrations/2024_09_14_170902_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('category')->nullable();
             $table->string('title')->nullable();
             $table->string('description')->nullable();
             $table->string('image')->nullable();
