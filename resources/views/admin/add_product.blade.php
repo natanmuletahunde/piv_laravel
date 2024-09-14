469a436673e66bcd4de4b1c3f9299980e0cf2a1e
pivnati <natanmuleta911@gmail.com>
2024-09-14 10:42:40 -0700

    create the the lable for the the product title

--- resources/views/admin/add_product.blade.php
+++ resources/views/admin/add_product.blade.php
@@ -1,13 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+@include('admin.css');
 </head>
 <body>
-    
+    @include('admin.header');
+    <div class="d-flex align-items-stretch">
+        <!-- Sidebar Navigation-->
+         @include('admin.sidebar');
+        <!-- Sidebar Navigation end-->
+        <div class="page-content">
+            <div class="page-header">
+                <div class="container-fluid">
 
- <h1>Add product </h1>
+            <div>
+
+            <form action="">
+                <div>
+                    <label> Product Title</label>
+                    <input type="text" name="title">
+                </div>
+            </form>
+            </div>
+            </div>
+        </div>
+    </div>
+  
+    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script> 
+    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"></script>
+    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
+    <script src="{{asset('admincss/js/front.js')}}"></script>
+
 </body>
+
 </html>
