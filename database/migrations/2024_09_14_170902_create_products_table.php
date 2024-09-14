f0e873b5a9027a1ef356b8f395a5d850fb5ab240
pivnati <natanmuleta911@gmail.com>
2024-09-14 10:12:09 -0700

    create the five items in the products

--- database/migrations/2024_09_14_170902_create_products_table.php
+++ database/migrations/2024_09_14_170902_create_products_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
-
+            $table->string('title')->nullable();
+            $table->string('title')->nullable();
+            $table->string('title')->nullable();
+            $table->string('title')->nullable();
             $table->timestamps();
         });
     }
