b2928267b3129f93592728e93b0f72be7cf1fd8a
pivnati <natanmuleta911@gmail.com>
2024-09-16 08:21:26 -0700

    add the images and form the pagination

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -74,7 +74,7 @@ class  AdminController extends Controller
   }
   public function view_product()
   {
-    $product = Product::all();
+    $product = Product::paginate(3);
     return view('admin.view_product',compact('product'));
   }
 }
--- resources/views/admin/view_product.blade.php
+++ resources/views/admin/view_product.blade.php
@@ -74,6 +74,7 @@
                     </tr>
                    @endforeach
                   </table>
+                  
                   </div>
             </div>
         </div>
