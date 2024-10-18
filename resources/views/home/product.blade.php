04c5427c9683f6f817e9ab7e4a1385f06462beed
pivnati <natanmuleta911@gmail.com>
2024-10-18 16:51:56 -0700

    make the products comdes from the home database in the home page

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -1,37 +1,36 @@
 <section class="shop_section layout_padding">
     <div class="container">
-      <div class="heading_container heading_center">
-        <h2>
-          Latest Products
-        </h2>
-      </div>
-      <div class="row">
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="images/p1.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  Ring
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $200
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
+        <div class="heading_container heading_center">
+            <h2>
+                Latest Products
+            </h2>
         </div>
-      </div>
-      
+        <div class="row">
+
+        @foreach ($product as $products)
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="box">
+                    <a href="">
+                        <div class="img-box">
+                            <img src="images/p1.png" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h6>
+                                <!-- Use $product here, not $products -->
+                                {{ $products->title }}
+                            </h6>
+                            <h6>
+                                Price
+                                <span>
+                                    <!-- Use $product here, not $products -->
+                                    $ {{ $products->price }}
+                                </span>
+                            </h6>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endforeach
+        </div>
     </div>
-  </section>
+</section>
