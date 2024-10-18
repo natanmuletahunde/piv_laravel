194eda528007423ae289146477740eb0a3e28702
pivnati <natanmuleta911@gmail.com>
2024-10-18 14:04:31 -0700

    modify the web.php dashboard to home.index

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", [HomeController::class,'home']);
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('home.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
