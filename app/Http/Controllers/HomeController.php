48008175cafa1cb3cc9d42ad1e61ab0ba3a31b1a
pivnati <natanmuleta911@gmail.com>
2024-10-18 19:31:48 -0700

    modify the header home button to redicreact to the home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,6 +26,6 @@ class HomeController extends Controller
 
     public function product_details($id){
         $data = Product::find($id);
-        return view("home.product_details");
+        return view("home.product_details",compact('data')) ;
     }
 }
--- resources/views/home/product_details.blade.php
+++ resources/views/home/product_details.blade.php
@@ -4,16 +4,83 @@
 
 <head>
 @include('home.css');
+<style>
+.div_center{
+display: flex;
+justify-content: center;
+align-items: center; 
+padding: 30px;
+}
+.detail-box{
+    padding:15px;
+}
+</style>
 </head>
 
 <body>
   <div class="hero_area">
        @include('home.header')
   </div>
-
+<!-- product details start  -->
    
-  
+<section class="shop_section layout_padding">
+    <div class="container">
+        <div class="heading_container heading_center">
+            <h2>
+                Latest Products
+            </h2>
+        </div>
+        <div class="row">
 
+       
+            <div class="col-md-12">
+                <div class="box">
+                
+                        <div class="div_center">
+                            <img width="400px" src="/products/{{$data->image}}" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h6>
+                                {{ $data->title }}
+                            </h6>
+                            <h6>
+                                Price
+                                <span> 
+                                    $ {{ $data->price }}
+                                </span>
+                            </h6>
+                        </div>
+
+
+                        <div class="detail-box">
+                            <h6>
+                              Category:  
+                                {{ $data->Category }}
+                            </h6>
+                            <h6>
+                                Available Quantity
+                                <span>
+                                    {{ $data->quantity }}
+                                </span>
+                            </h6>
+                        </div>
+
+                        <div class="detail-box">
+                
+                             <p>
+                                    {{ $data->description }}
+                                </p>
+                            </h6>
+                        </div>
+
+                </div>
+            </div>
+      
+        </div>
+    </div>
+</section>
+
+<!-- product details end  -->
    @include('home.footer')
 </body>
 
